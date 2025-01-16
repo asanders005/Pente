13faa9dea7d26a2ee344c40c9b5ba6f449a90851
Aiden Sanders <aidensandersbiz@gmail.com>
2025-01-16 09:37:11 -0700

    Add project files.

---
+++ Pente/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pente
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pente/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pente
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
