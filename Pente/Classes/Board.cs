1cd1f95caf31e444b7c10111fd09121e8344047c
Aiden Sanders <aidensandersbiz@gmail.com>
2025-01-16 09:53:04 -0700

    Initial Setup Game Board

---
+++ Pente/Classes/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Classes
+{
+    public class Board
+    {
+        public bool?[,] board { get; private set; }
+
+        public Board()
+        {
+            board = new bool?[19,19];
+        }
+
+        public void PlaceStone(int x, int y, bool isWhite)
+        {
+            if (board[x, y] == null)
+            {
+                board[x, y] = isWhite;
+            }
+        }
+
+        public void RemoveStone(int x, int y)
+        {
+            board[x, y] = null;
+        }
+    }
+}
---
+++ Pente/Classes/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Classes
+{
+    public class Game
+    {
+        public Board GameBoard { get; private set; }
+        public string[] Players { get; private set; }
+
+        public Game(string player1, string player2)
+        {
+            Players = new[] { player1, player2 };
+            GameBoard = new Board();
+        }
+    }
+}
